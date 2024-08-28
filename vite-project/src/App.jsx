fbfabc3035d248ecdd1f26d91d8f8823f8514702
Kiran755775 <segireddykiran123@gmail.com>
2024-08-28 19:18:05 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./assets/components/Home"
+function App() {
+  
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/assets/components/Home/index.jsx
@@ -0,0 +1,30 @@
+import { FiMenu } from "react-icons/fi";
+import { IoSearchSharp } from "react-icons/io5";
+import { BiCalendarEvent } from "react-icons/bi";
+import { FaCirclePlus } from "react-icons/fa6";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-[100vh] relative">
+        <div className="bg-[#5d138f] h-[12vh]">
+          <div className="flex justify-between py-2 px-2">
+            <div className="flex justify-around items-center w-[45vw]">
+              <FiMenu className="text-white text-2xl" />
+              <h1 className="text-white text-xl font-semibold">Transactions</h1>
+            </div>
+            <div className="flex items-center justify-around w-[20vw]">
+              <IoSearchSharp className="text-white text-2xl" />
+              <BiCalendarEvent className="text-white text-2xl" />
+            </div>
+          </div>
+          <ul className="flex justify-around items-center pt-2">
+            <li className="text-white  text-xs">TRANSACTIONS</li>
+            <li className="text-white  text-xs">TRANSFERS</li>
+          </ul>
+          <FaCirclePlus className=" absolute bottom-10 right-4 text-[#fc0377] text-6xl"/>
+        </div>
+      </div>
+    </>
+  );
+}
